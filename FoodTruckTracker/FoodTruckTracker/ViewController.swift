a394edff7b7bd40340712771de0fb1263dab1bc5
Tyler Christian <tylerc71197@gmail.com>
2020-01-06 15:00:21 -0500

    StoryBoard layout more than half done.

---
+++ FoodTruckTracker/FoodTruckTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTruckTracker
+//
+//  Created by Lambda_School_Loaner_218 on 1/6/20.
+//  Copyright © 2020 Lambda_School_Loaner_218. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
