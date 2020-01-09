d9f4612bec1a004b5d1a851fc7841a48e985efcb
Tyler Christian <tylerc71197@gmail.com>
2020-01-09 00:07:29 -0500

    Login set up

--- FoodTruckTracker/FoodTruckTracker/Controllers/RegisterViewController.swift
+++ FoodTruckTracker/FoodTruckTracker/Controllers/RegisterViewController.swift
@@ -24,6 +24,7 @@ class RegisterViewController: UIViewController {
     @IBOutlet weak var roleTextField: UITextField!
     @IBOutlet weak var cityTextField: UITextField!
     @IBOutlet weak var signUpButton: UIButton!
+    @IBOutlet weak var logintypeSegmentedControl: UISegmentedControl!
     
     
     @IBAction func LoginSelector(_ sender: UISegmentedControl) {
@@ -44,7 +45,7 @@ class RegisterViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         
     }
     
@@ -53,11 +54,10 @@ class RegisterViewController: UIViewController {
     
     
     @IBAction func registerPressed(_ sender: Any) {
-        
-       guard let email = emailTextField.text,
-        let password = passwordTextField.text,
-        let role = roleTextField.text,
-        let city = cityTextField.text else { return }
+        guard let email = emailTextField.text,
+            let password = passwordTextField.text,
+            let role = roleTextField.text,
+            let city = cityTextField.text else { return }
         let newUser = User(username: email, password: password, role: Int(role) ?? 2 , city: city)
         
         registerController.signUp(with: newUser) { (error) in
@@ -66,24 +66,51 @@ class RegisterViewController: UIViewController {
             }
             
             DispatchQueue.main.async {
-            self.performSegue(withIdentifier: "RegisterSegue", sender: self)
+                self.performSegue(withIdentifier: "RegisterSegue", sender: self)
             }
+            
+            if self.logintype == .signUp {
+                self.registerController.signUp(with: newUser) { (error) in
+                    if let error = error {
+                        print("Error occourred during sugn up: \(error)")
+                    } else {
+                        let alertControl = UIAlertController(title: "Sign up Succesful", message: "Now please login", preferredStyle: .alert)
+                        let alertAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+                        alertControl.addAction(alertAction)
+                        self.present(alertControl,animated: true) {
+                            self.logintype = .signUp
+                            self.logintypeSegmentedControl.selectedSegmentIndex = 1
+                            self.signUpButton.setTitle("Sign In", for: .normal)
+                        }
+                    }
+                }
+            } else {
+                self.registerController.login(with: newUser) { (error) in
+                    if let error = error {
+                        print("Error signing up: \(error)")
+                    } else {
+                        self.dismiss(animated: true, completion: nil)
+                    }
+                }
+            }
+            
+            
         }
-    }
         
-    
-    
-    
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
     }
-    */
-
+    
+    
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+    
 }
